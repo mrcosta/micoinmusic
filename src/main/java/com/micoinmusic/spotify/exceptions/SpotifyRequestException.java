932b903d494aa50a2e2d69c3f88e01781e1d188d
Mateus Costa <mateuscomp@gmail.com>
2017-08-17 22:32:34 -0300

    use code in spotify request exception

--- src/main/java/com/micoinmusic/spotify/exceptions/SpotifyRequestException.java
+++ src/main/java/com/micoinmusic/spotify/exceptions/SpotifyRequestException.java
@@ -2,9 +2,9 @@ package com.micoinmusic.spotify.exceptions;
 
 public class SpotifyRequestException extends RuntimeException {
     private int code;
-    private String message;
 
     public SpotifyRequestException(int code, String message) {
         super(message);
+        this.code = code;
     }
 }
