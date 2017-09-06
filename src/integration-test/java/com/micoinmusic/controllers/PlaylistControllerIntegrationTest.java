f13ea49d7c6455caa0afd3b44bd8cb4edf5b8a23
Mateus Costa <mateuscomp@gmail.com>
2017-09-06 18:12:53 -0300

    exlude logging dependencies that were conflicting

--- src/integration-test/java/com/micoinmusic/controllers/PlaylistControllerIntegrationTest.java
+++ src/integration-test/java/com/micoinmusic/controllers/PlaylistControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.micoinmusic.controllers;
 
+import net.codestory.http.WebServer;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,12 +25,17 @@ public class PlaylistControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
+    @Before
+    public void setUp() {
+        new WebServer().configure(routes -> routes.get("/", "hellow")).start(4040);
+    }
+
     @Test
     @Ignore
     public void shouldSayHi() throws Exception {
         String authToken = "BQAzjAmVDmUxeuOMqvD5wr-89-1E4Lof-UYr-RNlQ_INSID02tpIAouUSfg95tVPO6QGDCbNMldWv8VpCUVUC_ReOqV64AhhqLCIp-Yq8zCSK9WWWdNFs7qhjJdT7QmCHwXEhlC0lcK6Br65opcnXc9PB__fkSGOnvEu9GGd3ibCB2lwazo7jPwZ4mva3ce7ypLJKVeYNKoEqpsIRF5NxLLowiw9PaL-JIHgtE5dj-SQQJ0hQr14eXHiCiObys-too-EzDVSaInjnCeEjfjY0nqjbVpWKgX_hE4fQYBjy6ejFA";
 
-        mvc.perform(get("/artists?authToken=" + authToken).accept(APPLICATION_JSON))
+        mvc.perform(post("/playlists?authToken=" + authToken).accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("432")));
     }
