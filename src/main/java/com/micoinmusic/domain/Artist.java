8538fd9127d7848f1e56b3e99215bf4a16de80db
Mateus Costa <mateuscomp@gmail.com>
2017-09-20 11:25:14 -0300

    fix codacy issues and clean README.md

--- src/main/java/com/micoinmusic/domain/Artist.java
+++ src/main/java/com/micoinmusic/domain/Artist.java
@@ -3,17 +3,9 @@ package com.micoinmusic.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 @AllArgsConstructor
 public class Artist {
     private String name;
     private String id;
-    private List<Album> albums;
-
-    public Artist(String name, String id) {
-       this.name = name;
-       this.id = id;
-    }
 }
--- src/main/java/com/micoinmusic/spotify/deserializers/AlbumDeserializer.java
+++ src/main/java/com/micoinmusic/spotify/deserializers/AlbumDeserializer.java
@@ -9,13 +9,10 @@ import java.lang.reflect.Type;
 
 public class AlbumDeserializer implements JsonDeserializer<Album> {
 
-    private SpotifyAlbumJsonParser albumParser;
-    private SpotifyTracksJsonParser tracksParser;
-
     @Override
     public Album deserialize(JsonElement jsonElement, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        albumParser = new SpotifyAlbumJsonParser(jsonElement.getAsJsonObject());
-        tracksParser = new SpotifyTracksJsonParser(jsonElement.getAsJsonObject());
+        SpotifyAlbumJsonParser albumParser = new SpotifyAlbumJsonParser(jsonElement.getAsJsonObject());
+        SpotifyTracksJsonParser tracksParser = new SpotifyTracksJsonParser(jsonElement.getAsJsonObject());
 
         return new Album(albumParser.getName(), albumParser.getId(), albumParser.getReleaseDate(), tracksParser.getTracks());
     }
