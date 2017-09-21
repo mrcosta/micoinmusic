e3b33493fd6864eb33258b99d9a9da0fed395291
Mateus Costa <mateuscomp@gmail.com>
2017-09-21 14:13:21 -0300

    add unit test for playlistservice

--- src/test/java/com/micoinmusic/domain/services/PlaylistServiceTest.java
+++ src/test/java/com/micoinmusic/domain/services/PlaylistServiceTest.java
@@ -1,5 +1,28 @@
 package com.micoinmusic.domain.services;
 
+import com.micoinmusic.domain.Playlist;
+import com.micoinmusic.domain.Track;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class PlaylistServiceTest {
 
+    @Test
+    public void shouldCreatePlaylistForTheGivenYear() {
+        int currentYear = LocalDate.now().getYear();
+
+        Playlist playlist = mock(Playlist.class);
+        when(playlist.createPlaylist("randomAuthToken")).thenReturn(new Playlist("This 2017 in music", List.of(new Track("Music", "id", "Artist", "Album")), currentYear));
+        Playlist createdPlaylist = new PlaylistService(playlist).create("randomAuthToken");
+
+        assertThat(createdPlaylist.getName(), is("This 2017 in music"));
+        assertThat(createdPlaylist.getYear(), is(currentYear));
+    }
 }
